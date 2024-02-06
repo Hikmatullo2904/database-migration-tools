0a95e929b6d770ad3ed02eca3e81ec5c6a6771b8
Hikmatullo2904 <hikmatulloanvarbekov001@gmail.com>
2024-02-06 23:51:45 +0500

    project is initialized in git" ;

---
+++ src/main/java/com/hikmatullo/app/DatabasemigrationToolsApplication.java
@@ -0,0 +1,13 @@
+package com.hikmatullo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasemigrationToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabasemigrationToolsApplication.class, args);
+    }
+
+}
